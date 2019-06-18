33f115df6677eb4473fa09e9bdd5e04646a7f294
thanhtamphung <thtam.phung@gmail.com>
2019-06-18 15:29:04 +0200

    Update runskat.R

--- SKAT_R_vcf/runskat.R
+++ SKAT_R_vcf/runskat.R
@@ -4,31 +4,31 @@ args<-commandArgs(TRUE)
 # args[2]: gene_matrix
 # args[3]: gene_name
 # args[4]: file output
-myskat <- function(pheno_file,gene_matrix) {
+myskat <- function(pheno_file, gene_matrix) {
   library(SKAT)
   set.seed(1)
-  d<-read.table(pheno_file,header=TRUE,sep=" ")
+  d<-read.table(pheno_file, header=TRUE, sep=" ")
   # phenotype
   pheno<-d$PLq
   # import covariates to X
-  X<-matrix(c(d$age,d$sex),nrow=123,ncol=2,byrow=FALSE)
+  X<-matrix(c(d$age, d$sex), nrow=123, ncol=2, byrow=FALSE)
   
   # genotype matrix
-  Ztmp<-read.table(gene_matrix,header=TRUE,sep="\t")
-  Z<-data.matrix(Ztmp,rownames.force = NA)
+  Ztmp<-read.table(gene_matrix, header=TRUE, sep="\t")
+  Z<-data.matrix(Ztmp, rownames.force=NA)
   
   # run SKAT
-  obj<-SKAT_Null_Model(pheno ~ X, out_type="D",n.Resampling=0 , type.Resampling="bootstrap", Adjustment=TRUE)
-  skt = SKAT(Z, obj,kernel = "linear.weighted")
+  obj<-SKAT_Null_Model(pheno ~ X, out_type="D", n.Resampling=0, type.Resampling="bootstrap", Adjustment=TRUE)
+  skt = SKAT(Z, obj, kernel="linear.weighted")
   return(skt$p.value)
 }
 
 pheno_file<-args[1]
 gene_matrix<-args[2]
-p<-myskat(pheno_file,gene_matrix)
+p<-myskat(pheno_file, gene_matrix)
 gene_name<-args[3]
-gene<-data.frame(g=gene_name, pp = p)
-write.table(gene, file = args[4], sep= "\t",row.names = FALSE,col.names =FALSE, append = TRUE, quote=FALSE)
+gene<-data.frame(g=gene_name, pp=p)
+write.table(gene, file=args[4], sep="\t", row.names=FALSE, col.names=FALSE, append=TRUE, quote=FALSE)
 print(p)
 
 
